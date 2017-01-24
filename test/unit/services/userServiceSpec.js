af8248941e56ee84ce4bd67c5058921de21ddaa2
Balint Morvai <balint@morvai.de>
2017-01-24 17:12:40 +0100

    Implemented user service tests

---
+++ test/unit/services/userServiceSpec.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const userModel = require('../../../app/models/userModel');
+const userService = require('../../../app/services/userService');
+const ObjectId = require('mongoose').Types.ObjectId;
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const Promise = require('bluebird');
+chai.use(chaiAsPromised);
+
+
+describe('test/unit/services/userServiceSpec', function() { // eslint-disable-line
+  this.timeout(3000);
+  let userObj;
+  describe('createUser', () => {
+    beforeEach(() => {
+      userObj = {
+        id: new ObjectId(),
+        role: 1,
+        email: 'foo',
+        password: 'foobar42',
+      };
+    });
+    afterEach(() => {
+      userModel.findOne.restore();
+      userModel.createUser.restore();
+    })
+    it('should return new user object', () => {
+      sinon.stub(userModel, 'findOne', email => Promise.resolve(null));
+      sinon.stub(userModel, 'createUser', params => Promise.resolve(userObj));
+      const servicePromise = userService.createUser('foo', 'foobar42');
+      return expect(servicePromise).to.eventually.deep.equal(userObj);
+    });
+    it('should reject to create new user if email exists', () => {
+      sinon.stub(userModel, 'findOne', email => Promise.resolve(userObj));
+      sinon.stub(userModel, 'createUser', params => Promise.resolve(userObj));
+      const servicePromise = userService.createUser('foo', 'foobar42');
+      return expect(servicePromise).to.be.rejected;
+    });
+  });
+  describe('validateUser', () => {
+    beforeEach(() => {
+      userObj = {
+        id: new ObjectId(),
+        role: 1,
+        email: 'foo',
+        password: 'foobar42',
+      };
+    });
+    afterEach(() => {
+      userModel.findOne.restore();
+    })
+    it('should return user object', () => {
+      sinon.stub(userModel, 'findOne', email => Promise.resolve(userObj));
+      userObj.validatePassword = () => true;
+      const servicePromise = userService.validateUser('foo', 'foobar42');
+      return expect(servicePromise).to.be.resolved;
+    });
+    it('should reject validation if wrong email', () => {
+      sinon.stub(userModel, 'findOne', email => Promise.resolve(null));
+      userObj.validatePassword = () => true;
+      const servicePromise = userService.validateUser('foo', 'foobar42');
+      return expect(servicePromise).to.be.rejected;
+    });
+    it('should reject validation if wrong password', () => {
+      sinon.stub(userModel, 'findOne', email => Promise.resolve(userObj));
+      userObj.validatePassword = () => false;
+      const servicePromise = userService.validateUser('foo', 'foobar42');
+      return expect(servicePromise).to.be.rejected;
+    });
+  });
+  describe('findById', () => {
+    beforeEach(() => {
+      userObj = {
+        id: new ObjectId(),
+        role: 1,
+        email: 'foo',
+        password: 'foobar42',
+      };
+    });
+    afterEach(() => {
+      userModel.findById.restore();
+    })
+    it('should return user object', () => {
+      sinon.stub(userModel, 'findById', id => Promise.resolve(userObj));
+      const servicePromise = userService.findById(userObj.id);
+      return expect(servicePromise).to.eventually.deep.equal(userObj);
+    });
+    it('should reject if not found', () => {
+      sinon.stub(userModel, 'findById', id => Promise.resolve(null));
+      const servicePromise = userService.findById(userObj.id);
+      return expect(servicePromise).to.be.rejectedWith(Error, '404');
+    });
+  });
+  describe('updateById', () => {
+    beforeEach(() => {
+      userObj = {
+        role: 1,
+        email: 'foo',
+        password: 'foobar42',
+      };
+    });
+    afterEach(() => {
+      userModel.updateById.restore();
+    })
+    it('should be resolved', () => {
+      sinon.stub(userModel, 'updateById', (id, userObj) => Promise.resolve(1));
+      const servicePromise = userService.updateById(userObj.id, userObj);
+      return expect(servicePromise).to.be.resolved;
+    });
+    it('should reject if not found', () => {
+      sinon.stub(userModel, 'updateById', (id, userObj) => Promise.resolve(0));
+      const servicePromise = userService.updateById(userObj.id, userObj);
+      return expect(servicePromise).to.be.rejectedWith(Error, '404');
+    });
+  });
+  describe('deleteById', () => {
+    beforeEach(() => {
+      userObj = {
+        role: 1,
+        email: 'foo',
+        password: 'foobar42',
+      };
+    });
+    afterEach(() => {
+      userModel.deleteById.restore();
+    })
+    it('should be resolved', () => {
+      sinon.stub(userModel, 'deleteById', id => Promise.resolve(1));
+      const servicePromise = userService.deleteById(userObj.id);
+      return expect(servicePromise).to.be.resolved;
+    });
+    it('should reject if not found', () => {
+      sinon.stub(userModel, 'deleteById', id => Promise.resolve(0));
+      const servicePromise = userService.deleteById(userObj.id);
+      return expect(servicePromise).to.be.rejectedWith(Error, '404');
+    });
+  });
+});
