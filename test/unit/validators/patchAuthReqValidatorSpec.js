e0529fc6bb3eb2e653a22c931c81a4beab7c99f6
Balint Morvai <balint@morvai.de>
2017-01-24 21:38:56 +0100

    Implemented patch auth request validator tests

---
+++ test/unit/validators/patchAuthReqValidatorSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const patchReqValidator = require('../../../app/validators/patchAuthReqValidator');
+const config = require('../../../config/config');
+const expect = require('chai').expect;
+const _ = require('lodash');
+
+describe('test/unit/validators/patchReqValidatorSpec check', function() { // eslint-disable-line
+  let req = {};
+  beforeEach(() => {
+    req = {
+      token: 'dummyToken'
+    };
+  });
+  it('should accept only req with token', () => {
+    let error = patchReqValidator.check(req);
+    expect(error).to.be.equal(null);
+    delete req.token;
+    error = patchReqValidator.check(req);
+    expect(_.toString(error)).to.be.equal('VError: 400: token is required');
+  });
+});
