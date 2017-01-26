0fd21520f9eebc5ae687a144a7944f2e0cbbfaac
Balint Morvai <balint@morvai.de>
2017-01-26 18:47:46 +0100

    Implemented tests for post user request validator

---
+++ test/unit/validators/postUserReqValidatorSpec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const postReqValidator = require('../../../app/validators/postUserReqValidator');
+const config = require('../../../config/config');
+const expect = require('chai').expect;
+const _ = require('lodash');
+
+describe('test/unit/validators/postUserReqValidator check', function() { // eslint-disable-line
+  let req = {};
+  beforeEach(() => {
+    req = {
+      body: {
+        email: 'foo@google.com',
+        password: 'foobarfoobar',
+      },
+    };
+  });
+  it('should accept only valid email', () => {
+    let error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.empty;
+    delete req.body.email;
+    error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.equal('VError: 400: email is required');
+    req.body.email = 'foo';
+    error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.equal('VError: 400: valid email is required');
+  });
+  it('should accept only valid password', () => {
+    let error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.empty;
+    delete req.body.password;
+    error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.equal('VError: 400: password is required');
+    req.body.password = 'foo';
+    error = postReqValidator.check(req);
+    expect(_.toString(error)).to.be.equal(
+      `VError: 400: password has to have min ${config.passwordLength} chars`
+    );
+  });
+});
+
+describe('test/unit/validators/postUserReqValidator sanitize', function() { // eslint-disable-line
+  let req = {};
+  beforeEach(() => {
+    req = {
+      body: {
+        email: ' foo@google.com     ',
+        password: '   foobarfoobar ',
+      },
+    };
+  });
+  it('should sanitize req', () => {
+    postReqValidator.sanitize(req);
+    expect(req.body).to.deep.equal({
+      email: 'foo@google.com',
+      password: 'foobarfoobar',
+    });
+  });
+});
