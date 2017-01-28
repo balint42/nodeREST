96b09571cbf09cdd6b7d05485442b65563f439c6
Balint Morvai <balint@morvai.de>
2017-01-28 17:12:29 +0100

    users routes: new GET route;PATCH route validates;setMinRole bugfix

--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getDetailsUserReqValidator = require('../../validators/getDetailsReqValidator');
+const patchUserReqValidator = require('../../validators/patchUserReqValidator');
 const postUserReqValidator = require('../../validators/postUserReqValidator');
 const deleteUserReqValidator = require('../../validators/deleteReqValidator');
 const userService = require('../../services/userService');
@@ -10,6 +11,7 @@ const logger = require('../../../utils/logger');
 const config = require('../../../config/config');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 function createCheck(validator) {
   return (req, res, next) => {
@@ -25,10 +27,12 @@ function createCheck(validator) {
 
 const setMinimumRole = (req, res, next) => {
   // set required minimum role
-  if (req.params.userId == req.user.id) { // eslint-disable-line
-    req.minRole = config.roles.user;
+  if (req.user.id && _.toString(req.params.userId) === _.toString(req.user.id)) {
+    // user has to have min the role he tries to update to
+    req.minRole = req.params.role || config.roles.user;
   } else {
-    req.minRole = config.roles.manager;
+    // user has to have min the role he tries to update to
+    req.minRole = Math.max(config.roles.manager, req.params.role || config.roles.manager);
   }
   next();
 };
@@ -41,6 +45,27 @@ router.route('/users')
     passport.authenticate('local-signup', passportOpt),
     (req, res) => {
       res.status(200).json(req.user);
+    }
+  );
+
+// get users
+router.route('/users')
+  .get(
+    passport.authenticate('jwt-access', passportOpt),
+    setMinimumRole,
+    utils.requireRole(),
+    (req, res) => {
+      userService.findAll()
+        .then(users => {
+          res.json(users);
+        })
+        .catch(err => {
+          if (utils.isClientError(err)) {
+            return res.status(404).send();
+          }
+          logger.error(`Error getting users: ${utils.errorToString(err)}`);
+          res.status(500).send();
+        });
     }
   );
 
@@ -67,30 +92,32 @@ router.route('/users/:userId')
   );
 
 // update user
-router.route('/users/:userId').patch(
-  passport.authenticate('jwt-access', passportOpt),
-  setMinimumRole,
-  utils.requireRole(),
-  (req, res) => {
-    // no setting users to higher level than own level
-    if (config.env !== 'test') {
-      if (req.body.role > req.user.role) {
-        delete req.body.role;
-      }
-    }
-    userService.updateById(req.params.userId, req.body)
-      .then(() => {
-        res.status(204).send();
-      })
-      .catch(err => {
-        if (utils.isClientError(err)) {
-          return res.status(404).send();
+router.route('/users/:userId')
+  .patch(createCheck(patchUserReqValidator))
+  .patch(
+    passport.authenticate('jwt-access', passportOpt),
+    setMinimumRole,
+    utils.requireRole(),
+    (req, res) => {
+      // no setting users to higher level than own level
+      if (config.env !== 'test') {
+        if (req.body.role > req.user.role) {
+          delete req.body.role;
         }
-        logger.error(`Error updating user: ${utils.errorToString(err)}`);
-        res.status(500).send();
-      });
-  }
-);
+      }
+      userService.updateById(req.params.userId, req.body)
+        .then(() => {
+          res.status(204).send();
+        })
+        .catch(err => {
+          if (utils.isClientError(err)) {
+            return res.status(404).send();
+          }
+          logger.error(`Error updating user: ${utils.errorToString(err)}`);
+          res.status(500).send();
+        });
+    }
+  );
 
 // delete user
 router.route('/users/:userId')
